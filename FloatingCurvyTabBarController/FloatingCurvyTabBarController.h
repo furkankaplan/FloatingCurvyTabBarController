23b9bfab4da3f80e7448b5c66131b42ef904f4fd
Furkan KAPLAN <furkankaplan@outlook.com>
2020-03-06 00:03:48 +0300

    Initial Commit

---
+++ FloatingCurvyTabBarController/FloatingCurvyTabBarController.h
@@ -0,0 +1,19 @@
+//
+//  FloatingCurvyTabBarController.h
+//  FloatingCurvyTabBarController
+//
+//  Created by Furkan Kaplan on 6.03.2020.
+//  Copyright © 2020 Furkan Kaplan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FloatingCurvyTabBarController.
+FOUNDATION_EXPORT double FloatingCurvyTabBarControllerVersionNumber;
+
+//! Project version string for FloatingCurvyTabBarController.
+FOUNDATION_EXPORT const unsigned char FloatingCurvyTabBarControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatingCurvyTabBarController/PublicHeader.h>
+
+
